16223c16301152492c20b61a15c036993bd73ddb
Sidarta Rocha <sidarta_rocha@yahoo.com.br>
2017-03-17 13:16:59 -0300

    commit inicial do projeto da disciplina de compiladores20171

---
+++ src/parser/GrammarSymbols.java
@@ -0,0 +1,18 @@
+package parser;
+
+
+/**
+ * This class contains codes for each grammar terminal
+ * @version 2010-september-04
+ * @discipline Compiladores
+ * @author Gustavo H P Carvalho
+ * @email gustavohpcarvalho@ecomp.poli.br
+ */
+public class GrammarSymbols {
+
+	// Language terminals (starts from 0)
+	public static final int ID = 0;
+	//TODO
+	public static final int EOT = 1000;
+	
+}
